a1e5ccb1dc90a1f50a5ea69c1840d43481f5bdf3
Hugo Barona <hugofct20257@gmail.com>
2022-10-19 20:39:35 +0100

    az-204 m5 ex3

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require('http')
 const port = 80
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World!!!\n')
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.write('Hello AZ-204 Class!!!\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
