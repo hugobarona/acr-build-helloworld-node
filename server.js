139e1b4910cdb4d6d9fc980e4b3c229d6b74b40c
Hugo Barona <hugofct20257@gmail.com>
2023-03-06 09:38:59 +0000

    Az204 Feb 23 class

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.write('Hello AZ-204 Delloite Nov 22 Class!!!\n')
+  response.write('Hello AZ-204 Delloite Feb 23 Class!!!\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
